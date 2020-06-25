6b44f3a1ce8e8f465fd79f3597e67f4f1cad306b
Riad Mohamed <riaduk1@gmail.com>
2020-06-25 17:47:53 +0200

    Fix AB#55, Add the modules structure and their todos.
    Fix AB#46, fetchLocalTime is called from the fetchButtonTapped method
    
    Fix AB#47, the output string from fetchButtonTapped method is set to the localTimeLabel.text

--- FSA/Controller/ViewController.swift
+++ FSA/Controller/ViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     // MARK: - Outlets
     
-    @IBOutlet weak var countryTextField: UITextField!
+//    @IBOutlet weak var countryTextField: UITextField!
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var localTimeLabel: UILabel!
     
@@ -20,21 +21,69 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        countryTextField.delegate = self
+//        countryTextField.delegate = self
         cityTextField.delegate = self
     }
 
+    // MARK: - Functions
+    
+    func fetchLocalTime(address locationString: String) -> String {
+        
+        //TODO: Get coordinates of the city.
+        let coordinates = getCoordinatesByCoreLocation(address: locationString)
+        //TODO: Get the current unix time from the TimeZoneDB API
+        let currentUnixTime = getUnixTime(lat: Double(coordinates.latitude), long: Double(coordinates.longitude))
+        //TODO: convert the unix time to string "HH:MM:SS AM/PM"
+        let currentLocalTimeString: String = getLocalTimeString(unix: currentUnixTime)
+        
+        return currentLocalTimeString
+    }
+    
+    func getCoordinatesByCoreLocation(address: String) -> CLLocationCoordinate2D {
+        // TODO: Initiate the CoreLocation service to get the coordinates of the given address.
+        
+        var coordinates = CLLocationCoordinate2D()
+        return coordinates
+    }
+    
+    func getUnixTime(lat: Double, long: Double) -> Double {
+        var unixTime: Double = 0
+        // TODO: Create the networking code for the request and receieve the response.
+        
+        // TODO: Parse the response as JSON and extract the current UNIX time.
+        
+        return unixTime
+    }
+    
+    func getLocalTimeString(unix: Double) -> String {
+        var localTimeString = ""
+        // TODO: Initiate the Date Formatter object with the time style.
+        
+        // TODO: Get the date with the corressponding given unix time.
+        
+        // TODO: return the output as a string.
+        return localTimeString
+    }
+    
+    
     // MARK: - Actions
         // MARK: - FetchButtonTapped
     @IBAction func fetchButtonTapped(_ sender: UIButton) {
-        countryTextField.endEditing(true)
+//        countryTextField.endEditing(true)
         cityTextField.endEditing(true)
         print("Button Tapped")
+        print(cityTextField.text ?? "")
         // TODO: call the fetch function
+        if let locationString = cityTextField.text, locationString.count != 0 {
+            localTimeLabel.text = fetchLocalTime(address: locationString)
+        }
+        
+        
     }
 
 }
 
+// MARK: - Delegates Methods
 // MARK: - UITextFieldDelegate
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
