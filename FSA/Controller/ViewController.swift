55bb9343bac40a9f2d53e850498aa0bbc72eefb9
Riad Mohamed <riaduk1@gmail.com>
2020-06-30 15:54:25 +0200

    Fix AB#56. Re-order and add documentation of the functions in ViewController.swift

--- FSA/Controller/ViewController.swift
+++ FSA/Controller/ViewController.swift
@@ -27,26 +27,24 @@ class ViewController: UIViewController {
     }
 
     // MARK: - Functions
-    
-    func fetchLocalTime(address locationString: String) -> String {
+    /// Takes unix time and create a Date Formatter object that will parse the unix time into string and return it back.
+    /// - Parameter unix: Unix time
+    /// - Returns: string with the format of "HH:MM:SS AM/PM"
+    func getLocalTimeString(unix: Double) -> String {
+        var localTimeString = "EMPTY TIME STRING"
+        // TODO: Initiate the Date Formatter object with the time style.
         
-        //TODO: Get coordinates of the city.
-        let coordinates = getCoordinatesByCoreLocation(address: locationString)
-        //TODO: Get the current unix time from the TimeZoneDB API
-        let currentUnixTime = getUnixTime(lat: Double(coordinates.latitude), long: Double(coordinates.longitude))
-        //TODO: convert the unix time to string "HH:MM:SS AM/PM"
-        let currentLocalTimeString: String = getLocalTimeString(unix: currentUnixTime)
+        // TODO: Get the date with the corressponding given unix time.
         
-        return currentLocalTimeString
+        // TODO: return the output as a string.
+        return localTimeString 
     }
     
-    func getCoordinatesByCoreLocation(address: String) -> CLLocationCoordinate2D {
-        // TODO: Initiate the CoreLocation service to get the coordinates of the given address.
-        
-        var coordinates = CLLocationCoordinate2D()
-        return coordinates
-    }
-    
+    /// Takes a lat and long, create a GET request to TimeZone DB with given coordinates, receieve the JSON response then extract and return the unix time for the current local time at those coordinates.
+    /// - Parameters:
+    ///   - lat: latitude of the location
+    ///   - long: longitude of the location
+    /// - Returns: current unix local time of the given location
     func getUnixTime(lat: Double, long: Double) -> Double {
         var unixTime: Double = 0
         // TODO: Create the networking code for the request and receieve the response.
@@ -56,16 +54,32 @@ class ViewController: UIViewController {
         return unixTime
     }
     
-    func getLocalTimeString(unix: Double) -> String {
-        var localTimeString = ""
-        // TODO: Initiate the Date Formatter object with the time style.
+    
+    /// Takes a location address string and returns the coordinates of that address.
+    /// - Parameter address: string describing the location address in the form of "{cityName}" or  "{cityName}, {countryName}"
+    /// - Returns: CLLocationCoordinate2D object containg the coordinates of the location address given
+    func getCoordinatesByCoreLocation(address: String) -> CLLocationCoordinate2D {
+        // TODO: Initiate the CoreLocation service to get the coordinates of the given address.
         
-        // TODO: Get the date with the corressponding given unix time.
-        
-        // TODO: return the output as a string.
-        return localTimeString 
+        var coordinates = CLLocationCoordinate2D()
+        return coordinates
     }
     
+    /// Take a location address string and returns the local time of that location using CoreLocation and API from TimeZoneDB
+    /// - Parameter locationString: string describing the location address in the form of "{cityName}" or  "{cityName}, {countryName}"
+    /// - Returns: string with the current local time at the given location address in the form of "HH:MM:SS AM/PM"
+    func fetchLocalTime(address locationString: String) -> String {
+        //TODO: Get coordinates of the city.
+        let coordinates = getCoordinatesByCoreLocation(address: locationString)
+        
+        //TODO: Get the current unix time from the TimeZoneDB API
+        let currentUnixTime = getUnixTime(lat: Double(coordinates.latitude), long: Double(coordinates.longitude))
+        
+        //TODO: convert the unix time to string "HH:MM:SS AM/PM"
+        let currentLocalTimeString: String = getLocalTimeString(unix: currentUnixTime)
+        
+        return currentLocalTimeString
+    }
     
     // MARK: - Actions
         // MARK: - FetchButtonTapped
@@ -85,8 +99,8 @@ class ViewController: UIViewController {
             print("Country textfield is empty, couldn't get country name")
         }
         print(locationString)
-        // TODO: call the fetchLocalTime function
-        fetchLocalTime(address: locationString)
+        // TODO: call the fetchLocalTime function and set the localTimeLabel.text to it's result
+        localTimeLabel.text = fetchLocalTime(address: locationString)
     }
 
 }
