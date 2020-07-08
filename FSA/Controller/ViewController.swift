6eff6d18b31859ab422c5aff0cedf59acf87542d
Riad Mohamed <riaduk1@gmail.com>
2020-07-08 14:43:22 +0200

    Fix AB#68. Refactor some of the functions.
    + Add the ability to look for local time of a country not just a city.

--- FSA/Controller/ViewController.swift
+++ FSA/Controller/ViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController {
-    // MARK: - Outlets
-    
+    // MARK: - Properties
     var localTime: String = "" {
         didSet {
             DispatchQueue.main.async {
@@ -19,11 +18,14 @@ class ViewController: UIViewController {
             }
         }
     }
+    
     var state: State = .idle {
         didSet {
             print(state)
         }
     }
+    
+    // MARK: - Outlets
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var countryTextField: UITextField!
     @IBOutlet weak var localTimeLabel: UILabel!
@@ -32,8 +34,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //        countryTextField.delegate = self
         cityTextField.delegate = self
         countryTextField.delegate = self
         state = .idle
@@ -43,11 +43,11 @@ class ViewController: UIViewController {
     /// Takes unix time and create a Date Formatter object that will parse the unix time into string and return it back.
     /// - Parameter unix: Unix time
     /// - Returns: string with the format of "HH:MM:SS AM/PM"
-    func formatUnixTime(unixTime: Double, gmtOffset: Int) -> String {
+    func formatUnixTime(unixTime: Double) -> String {
         state = .formattingUnixTime
-        // TODO: Initiate the Date Formatter object with the time style.
+        
         let date = Date(timeIntervalSince1970: unixTime)
-        print(date)
+        
         let dateFormatter = DateFormatter()
         dateFormatter.timeZone = TimeZone(abbreviation: "GMT")!
         dateFormatter.timeStyle = .medium
@@ -58,6 +58,7 @@ class ViewController: UIViewController {
         self.state = .idle
         return localTimeString 
     }
+    
     func parseJSON(timeData: Data) -> TimeModel? {
         let decoder = JSONDecoder()
         do {
@@ -66,10 +67,7 @@ class ViewController: UIViewController {
         } catch {
             print(error)
             return nil
-            
         }
-        
-        
     }
     
     
@@ -79,14 +77,15 @@ class ViewController: UIViewController {
     ///   - long: longitude of the location
     /// - Returns: current unix local time of the given location
     func getUnixTime(coordinates: CLLocationCoordinate2D, completionHander: @escaping (TimeModel?, Error?) -> Void) {
-        // TODO: Create the networking code for the request and receieve the response.
+        
         state = .gettingUnixTime
         let APIKey = "AP6KGMZ6GGA3"
         let format = "json"
         let by = "position"
+
         let timeZoneURL = URL(string: "https://api.timezonedb.com/v2.1/get-time-zone?key=\(APIKey)&format=\(format)&by=\(by)&lat=\(coordinates.latitude)&lng=\(coordinates.longitude)")
         
-        // Create a URL
+        // 1- Create a URL
         guard let url = timeZoneURL else {
             print("Creating URL failed")
             state = .idle
@@ -94,10 +93,10 @@ class ViewController: UIViewController {
             return
         }
         
-        // Create URLSession
+        // 2- Create URLSession
         let session = URLSession(configuration: .default)
         
-        // Give the session a task
+        // 3- Give the session a task
         let task = session.dataTask(with: url) { (data, response, error) in
             if error != nil {
                 print("Networking code returned an error. \(error!)")
@@ -116,7 +115,8 @@ class ViewController: UIViewController {
             self.state = .idle
             completionHander(timeModel, nil)
         }
-        // Start the task
+        
+        // 4- Start the task
         task.resume()
     }
     
@@ -144,17 +144,16 @@ class ViewController: UIViewController {
     /// - Parameter locationString: string describing the location address in the form of "{cityName}" or  "{cityName}, {countryName}"
     /// - Returns: string with the current local time at the given location address in the form of "HH:MM:SS AM/PM"
     func fetchLocalTime(address locationString: String) {
-        // CoreLocation Code
+        // CoreLocation Module.
         getCoordinatesByCoreLocation(address: locationString) { (c, error) in
             guard let coordinates = c, error == nil else {
                 print(error!)
                 return
             }
-            
             print("Phase one result = \(coordinates)")
             self.localTime = "\(coordinates.latitude), \(coordinates.longitude)"
             
-            // Networking code
+            // Networking Module.
             self.getUnixTime(coordinates: coordinates) { (timeModel, error) in
                 if error != nil {
                     print(error!)
@@ -166,8 +165,8 @@ class ViewController: UIViewController {
                 print("Phase two result = \(safeTimeModel)")
                 self.localTime = "\(safeTimeModel.timestamp)"
                 
-                //TODO: UNIX Formatting code
-                self.localTime = self.formatUnixTime(unixTime: safeTimeModel.timestamp, gmtOffset: safeTimeModel.gmtOffset)
+                // UNIX Formatting Module.
+                self.localTime = self.formatUnixTime(unixTime: safeTimeModel.timestamp)
             }
         }
     }
@@ -175,32 +174,36 @@ class ViewController: UIViewController {
     // MARK: - Actions
     // MARK: - FetchButtonTapped
     func setupLocationString() -> String? {
-        guard let cityName = cityTextField.text, cityName.count != 0 else {
-            print("City textfield is empty, couldn't get city name")
-            return nil
-        }
         
-        var locationString = cityName
-        if let countryName = countryTextField.text, countryName.count != 0 {
-            locationString.append(", " + countryName)
-        } else {
-            print("Country textfield is empty, couldn't get country name")
-        }
+        var locationString = ""
         
+        if let cityName = cityTextField.text, let countryName = countryTextField.text {
+            if cityName.count != 0 {
+                locationString = cityName
+            } else if countryName.count != 0 {
+                locationString = "\(cityName), \(countryName)"
+            } else {
+                if countryName.count != 0 {
+                    locationString = countryName
+                }
+            }
+        }
         return locationString
     }
     
     @IBAction func fetchButtonTapped(_ sender: UIButton) {
+        // Hide the keyboard.
         countryTextField.endEditing(true)
         cityTextField.endEditing(true)
         
+        // Get a locationString.
         guard let locationString = setupLocationString() else {
             print("Could not get the location string")
             return
         }
         
+        // Fetch the local time.
         fetchLocalTime(address: locationString)
-        
     }
     
 }
@@ -211,7 +214,7 @@ extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.endEditing(true)
         print(textField.text ?? "")
-        // TODO: Call the fetch function
+        fetchButtonTapped(UIButton())
         return true
     }
     
