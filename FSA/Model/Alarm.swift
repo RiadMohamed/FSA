d70d049918304a98bea108e671720bc652f56638
Riad Mohamed <riaduk1@gmail.com>
2020-09-19 22:28:18 +0200

    Create a primitive Alarm class

---
+++ FSA/Model/Alarm.swift
@@ -0,0 +1,16 @@
+//
+//  Alarm.swift
+//  FSA
+//
+//  Created by Riad Mohamed on 9/19/20.
+//  Copyright © 2020 Riad Mohamed. All rights reserved.
+//
+
+import Foundation
+
+struct Alarm {
+    var alarmDate: Date
+    var title: String
+    var notes: String
+    
+}
