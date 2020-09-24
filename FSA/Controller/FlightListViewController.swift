47426688e558dd108ed6b6235e2845260a992e50
Riad Mohamed <riaduk1@gmail.com>
2020-09-24 17:00:35 +0200

    Add a viewController to the SB.
    Add the relationship between the new VC and the TabBarController.
    
    Create and add a FlightListVC file.
    
    Conform the new VC to be a subclass of FlightListVC

---
+++ FSA/Controller/FlightListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FlightListViewController.swift
+//  FSA
+//
+//  Created by Riad Mohamed on 9/24/20.
+//  Copyright © 2020 Riad Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class FlightListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
