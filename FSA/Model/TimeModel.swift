226935215fc42406177b4b6f07d1fa3bd6831c21
Riad Mohamed <riaduk1@gmail.com>
2020-10-03 22:06:27 +0200

    Flight Schedule feature is now complete. Close #18

---
+++ FSA/Model/TimeModel.swift
@@ -0,0 +1,13 @@
+//
+//  TimeModel.swift
+//  FSA
+//
+//  Created by Riad Mohamed on 7/5/20.
+//  Copyright © 2020 Riad Mohamed. All rights reserved.
+//
+
+import Foundation
+struct TimeModel: Decodable {
+    let timestamp: Double
+    let gmtOffset: Int
+}
